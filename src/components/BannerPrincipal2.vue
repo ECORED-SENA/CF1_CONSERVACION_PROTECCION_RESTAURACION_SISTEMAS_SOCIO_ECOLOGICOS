6add20c10588b2bc3b43cf5a9d4cbefd5d991ac2
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2022-04-21 17:08:03 -0500

    Ajuste Banner

--- src/components/BannerPrincipal2.vue
+++ src/components/BannerPrincipal2.vue
@@ -16,13 +16,7 @@
             i.fas.fa-angle-right
 
       .d-none.d-lg-block.col-lg-5.px-0.banner-principal__img
-        img(:src="globalData.imagenBannerPrincipal" style="width: 350px; margin-left:100px;")
-    //--.imagen_flotante_1.d-none.d-xl-block: img(src="@/assets/curso/inicio/float1.svg")
-    //--.imagen_flotante_2.d-none.d-xl-block: img(src="@/assets/curso/inicio/float2.svg")
-    //--.imagen_flotante_3.d-none.d-xl-block: img(src="@/assets/curso/inicio/float3.svg")
-    //--.imagen_flotante_4.d-none.d-xl-block: img(src="@/assets/curso/inicio/float4.svg")
-    //--.imagen_flotante_5.d-none.d-xl-block: img(src="@/assets/curso/banner-principal.png")
-  
+        img.img-a(:src="globalData.imagenBannerPrincipal" style="width: 350px")
 
 </template>
 
@@ -86,7 +80,7 @@ export default {
         .banner-principal__img
           padding-right: 3rem!important
   &__img
-    animation: scale 2s ease-in-out infinite alternate
+    //animation: scale 2s ease-in-out infinite alternate
     @if $banner-principal-img-y == 'arriba'
       align-self: flex-start
       padding-bottom: 1.5rem
